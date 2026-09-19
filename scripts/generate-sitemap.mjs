dce5b98aeb01c13d784cca046bb524eec5a75c4c
mohammad <mohammad.jamali.mj1385@gmail.com>
2026-09-19 14:46:32 +0330

    done url

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -66,7 +66,7 @@ async function generate() {
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map(u => `  <url>\n    <loc>${escapeXml(u.loc)}</loc>\n    <priority>${u.priority}</priority>\n  </url>`).join('\n')}
+${urls.map(u => `  <url>\n    <loc>${u.loc.replace(/&/g, '&amp;')}</loc>\n    <priority>${u.priority}</priority>\n  </url>`).join('\n')}
 </urlset>`;
 
   writeFileSync('public/sitemap.xml', xml, 'utf-8');
